3e977f49acf908f7e333afb28f9cccfaec803fe3
anishamahato <anisha.mahato11@gmail.com>
2026-08-29 11:56:32 +0530

    Update and rename VAE.py to VAE and classification head.py

--- VAE and classification head.py
+++ VAE and classification head.py
@@ -161,3 +161,22 @@ class VAEDecoder(nn.Module):
         reconstruction = self.decoder(x)
 
         return reconstruction
+
+
+class ClassificationHead(nn.Module):
+
+    def __init__(self, latent_dim=256, num_classes=25):
+        super().__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, z):
+
+        logits = self.classifier(z)
+
+        return logits
