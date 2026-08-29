0cd1041b4e06f1d0aef371face0dfb3ca8e625b4
anishamahato <anisha.mahato11@gmail.com>
2026-08-29 17:55:04 +0530

    Update VAE Implementation.py

--- VAE Implementation.py
+++ VAE Implementation.py
@@ -163,20 +163,4 @@ class VAEDecoder(nn.Module):
         return reconstruction
 
 
-class ClassificationHead(nn.Module):
 
-    def __init__(self, latent_dim=256, num_classes=25):
-        super().__init__()
-
-        self.classifier = nn.Sequential(
-            nn.Linear(latent_dim, 128),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(128, num_classes)
-        )
-
-    def forward(self, z):
-
-        logits = self.classifier(z)
-
-        return logits
