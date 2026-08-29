2598189a5377e040a43ee0340e1852abf31171f4
anishamahato <anisha.mahato11@gmail.com>
2026-08-29 17:55:20 +0530

    Update ClassificationHead.py

--- ClassificationHead.py
+++ ClassificationHead.py
@@ -1,1 +1,17 @@
+class ClassificationHead(nn.Module):
 
+    def __init__(self, latent_dim=256, num_classes=25):
+        super().__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, z):
+
+        logits = self.classifier(z)
+
+        return logits
